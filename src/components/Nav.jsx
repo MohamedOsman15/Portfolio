fdff2b26da678d29be5c1b95dd7ca7f8d23b0a7e
MohamedOsman15 <osman.mohamed2244@gmail.com>
2023-01-09 17:11:04 -0600

    styling

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,7 +9,7 @@ const Nav = () => {
             <div className="navigation">
                 <NavLink to="/about" className={({ isActive }) => (isActive ? "link-active" : "navLinks")}><h2>About Me</h2></NavLink>
                 <NavLink to="/projects" className={({ isActive }) => (isActive ? "link-active" : "navLinks")}><h2>Projects</h2></NavLink>
-                <a className="navLinks" target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/1j-O1gi_dqOeUpq31YQ2F0szdjtfDIdEBdZKh3kt3CsY/edit?usp=sharing"><h2>Resume</h2></a>
+                <a className="navLinks" target="_blank" rel="noreferrer" href="https://docs.google.com/document/d/14VMhTJaiTcUWomMeezH03_0vcLRVj7QwhuPnhu347MI/edit?usp=sharing"><h2>Resume</h2></a>
             </div>
         </div>
     )
